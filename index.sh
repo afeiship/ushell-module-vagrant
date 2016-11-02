6b67ed76e086c32f0f893b8f3526241c52fa99e1
feizheng <1290657123@qq.com>
2016-11-02 14:12:25 +0800

    intiial

---
+++ index.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+alias vg-box-add='vagrant box add';
+alias vg-init='vagrant init';
+alias vg-up='vagrant up';
+
+unset ROOT_PATH;
