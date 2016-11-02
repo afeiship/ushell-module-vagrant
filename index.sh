e8ab058ebb68d8088d77e1439f06853ee6fdf58c
feizheng <1290657123@qq.com>
2016-11-02 14:33:14 +0800

    box list

--- index.sh
+++ index.sh
@@ -3,6 +3,8 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias vg-box-add='vagrant box add';
+alias vg-box-list='vagrant box list';
+alias vg-box-remove='vagrant box remove';
 alias vg-init='vagrant init';
 alias vg-up='vagrant up';
 
