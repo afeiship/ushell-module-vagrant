e97b15ad64a0508a4e1e2fc3331d4390c8a299fe
afeiship <1290657123@qq.com>
2019-04-20 11:59:50 +0800

    fix: add vg

--- index.sh
+++ index.sh
@@ -5,6 +5,7 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias vg-box-add='vagrant box add';
 alias vg-box-list='vagrant box list';
 alias vg-box-remove='vagrant box remove';
+alias vg='vagrant';
 alias vg-init='vagrant init';
 alias vg-up='vagrant up';
 alias vg-ssh='vagrant ssh';
